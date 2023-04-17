268a517bc55847f437260ecd3b1399f19a327e86
abdsat@pm.me <abdsat@pm.me>
2023-04-17 05:46:19 +0300

    css fix

--- sw.js
+++ sw.js
@@ -14,16 +14,14 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v1';
+const PRECACHE = 'precache-v1.1';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'index.html',
   './', // Alias for index.html
-  'styles.css',
-  '../../styles/main.css',
-  'demo.js'
+  'style.css',
+  'kufi.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
